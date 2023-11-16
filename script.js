8ff9f9a32fa2631aaf39b25bd97a200040154ff5
Jao16905 <Jao16905>
2023-11-16 11:29:59 -0500

    Cria função de conclusão e exclusão de atividade

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputTask = document.querySelector("#inputTask");
 const addTask = document.querySelector("#addTask");
 const todoUl = document.querySelector("#taskUl");
+const todoList = document.querySelectorAll("li")
 const TODO = "TodoList";
 const firebaseConfig = {
     apiKey: "AIzaSyCXutUH_cgA_g9SMdNANa6AiKgL-ZJbhBs",
@@ -15,7 +16,8 @@ const firebaseConfig = {
   
 let db = firebase.firestore();
 
-loadElements()
+document.addEventListener("DOMContentLoaded", loadElements)
+
 function createTask(){
     if(inputTask.value != ""){
     let taskLi = document.createElement("li");
@@ -28,7 +30,7 @@ function createTask(){
     taskLi.appendChild(checkBtn);
     taskLi.appendChild(trashBtn);
 
-    taskLi.classList.add("d-flex" ,"justify-content-center")
+    taskLi.classList.add("d-flex" ,"justify-content-center", "mb-3")
     taskText.classList.add("fs-5" ,"align-items-center" ,"d-flex" , "p-2");
     checkBtn.classList.add("check-btn", "btn", "btn-success", "rounded-0");
     trashBtn.classList.add("close-btn", "btn", "btn-danger", "rounded-0");
@@ -36,21 +38,24 @@ function createTask(){
     taskText.textContent = inputTask.value;
     checkBtn.innerHTML = '<i class="bi bi-check-lg"></i>';
     trashBtn.innerHTML = '<i class="bi bi-trash"></i>';
+    checkBtn.addEventListener("click",checkElement);
+    trashBtn.addEventListener("click",deleteElements)
     
     let liProps ={
         id: Math.random(),
         text: inputTask.value,
         check: false
     }
+    trashBtn.id = liProps.id
     saveElements(liProps)
+    taskLi.id = liProps.id
+     console.log(taskLi.id)
     }
     else{
         alert("Insira alguma atividade")
     }
-
     inputTask.value=""
 }
-
 
 function saveElements(liElement){
 
@@ -66,8 +71,17 @@ function saveElements(liElement){
 
 }
 
-function checkElements(e){
-    console.log(e.target)
+function deleteElements(e){
+    const item = e.target;
+    let elementId = item.parentElement.parentNode.id
+
+    
+    db.collection(TODO).where("id", "==", parseFloat(elementId)).get().then(snapshot=>{
+        snapshot.forEach(doc=>{
+            doc.ref.delete().then(()=>item.parentElement.parentNode.remove())
+        })
+    })
+    
 }
 
 function loadElements(){
@@ -82,7 +96,7 @@ function loadElements(){
     taskLi.appendChild(checkBtn);
     taskLi.appendChild(trashBtn);
 
-    taskLi.classList.add("d-flex" ,"justify-content-center")
+    taskLi.classList.add("d-flex" ,"justify-content-center","mb-3")
     taskText.classList.add("fs-5" ,"align-items-center" ,"d-flex" , "p-2");
     checkBtn.classList.add("check-btn", "btn", "btn-success", "rounded-0");
     trashBtn.classList.add("close-btn", "btn", "btn-danger", "rounded-0");
@@ -90,9 +104,44 @@ function loadElements(){
     taskText.textContent = doc.data().text
     checkBtn.innerHTML = '<i class="bi bi-check-lg"></i>';
     trashBtn.innerHTML = '<i class="bi bi-trash"></i>';
+
+    checkBtn.addEventListener("click",checkElement);
+    trashBtn.addEventListener("click",deleteElements);
+    
+    let liProps ={
+        id: doc.data().id,
+        text: inputTask.value,
+        check: doc.data().check
+    }
+    taskLi.classList.toggle("check", doc.data().check)
+    taskLi.id = liProps.id
     }))
 }
+
+function checkElement(e){
+    const item = e.target
+    let elementId = item.parentElement.parentNode.id
+
+        db.collection(TODO).where("id", "==", parseFloat(elementId)).get().then(snapshot =>{
+            snapshot.forEach(doc =>{
+                const newCheckStatus = !doc.data().check
+                doc.ref.update({
+                    check:newCheckStatus
+                })
+                .then(() => item.parentElement.parentNode.classList.toggle("check", newCheckStatus))
+
+            });
+        });
+     
+        
+};
 addTask.addEventListener("click", createTask);
+
+
+
+
+
+
 
 
  
